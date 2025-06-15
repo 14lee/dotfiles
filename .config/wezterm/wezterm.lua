5a4b461eea31c4398094c1ebcacb9b48326a6ade
lyd <liyudong@southzn.com>
2025-06-15 19:27:07 +0800

    Feat: update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,34 +1,68 @@
 local wezterm = require("wezterm")
-local key_configs = require("wezterm_keys")
+local act = wezterm.action
+local config = wezterm.config_builder()
 
-local config = {
-  font_size = 18,
-  font = wezterm.font {
-    family = "Maple Mono NF CN",
-    harfbuzz_features = { "calt=0", "zero=1", "cv01=1"},
-  },
-  color_scheme = "Catppuccin Mocha",
+local custom_config = require('configs.bindings')
+local gpu_adapters = require('utils.gpu-adapter')
 
-  automatically_reload_config = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-  window_decorations = "RESIZE",
-  show_new_tab_button_in_tab_bar = false,
-  window_background_opacity = 0.8,
-  macos_window_background_blur = 15,
-  text_background_opacity = 0.9,
-  adjust_window_size_when_changing_font_size = false,
-  default_cursor_style = "BlinkingBar", -- 光标样式
-  window_padding = {
-    left = 20,
-    right = 20,
-    bottom = 5,
-    top = 20,
-  },
-
-  -- leader = key_configs.leader,
-  -- keys = key_configs.keys,
-  -- key_tables = key_configs.key_tables,
+-- font
+config.font_size = 18
+config.font = wezterm.font {
+  family = "Maple Mono NF CN",
+  harfbuzz_features = { "calt=0", "zero=1", "cv01=1"}
 }
+
+-- color scheme
+config.color_scheme = "Catppuccin Mocha"
+
+-- gpu
+config.front_end = 'WebGpu'
+webgpu_power_preference = 'HighPerformance'
+webgpu_preferred_adapter = gpu_adapters:pick_best()
+
+-- appearance
+config.automatically_reload_config = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.show_new_tab_button_in_tab_bar = false
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 15
+config.text_background_opacity = 0.9
+config.adjust_window_size_when_changing_font_size = false
+
+-- cursor
+config.default_cursor_style = "BlinkingBar"
+config.animation_fps = 120
+config.cursor_blink_ease_in = 'EaseOut'
+config.cursor_blink_ease_out = 'EaseOut'
+config.cursor_blink_rate = 650
+
+-- launch
+config.launch_menu = {
+  {
+    args = { 'zsh', '-l' },
+    label = 'Zsh',
+  },
+}
+
+config.window_padding = {
+  left = 20,
+  right = 20,
+  bottom = 5,
+  top = 5,
+}
+
+config.window_close_confirmation = 'NeverPrompt'
+
+visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 250,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 250,
+  target = 'CursorColor',
+}
+
+config.keys = custom_config.keys
 
 return config
